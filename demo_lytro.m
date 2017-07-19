e29759f0923ed353ce8d35c4e4dde0f91bf9901c
jiaying1992 <1439352516@qq.com>
2017-07-19 08:41:42 +0800

    Delete demo_lytro.m

--- demo_lytro.m
+++
@@ -1,7 +0,0 @@
-clc
-clear
-file_path     = 'F:/ICME2017/Code/input/tao_dataset/IMG_2.jpg';
-data          = (imread(file_path)) ;    
-depth_output  = computeDepth_lytro(data)  ;
-figure; 
-imshow(depth_output);
