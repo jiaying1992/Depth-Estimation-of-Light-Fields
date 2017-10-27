1d21a0a070176a29132b5c4364714fee4cfb694a
jiaying1992 <1439352516@qq.com>
2017-10-27 11:15:29 +0800

    Update demo_lytro.m

--- demo_lytro.m
+++ demo_lytro.m
@@ -1,12 +1,24 @@
 %% Depth estimation on a real world image taken by Lytro camera
 %  author: Ying Jia
 %  contact: jyustc93@mail.ustc.edu.cn
-%  time: 20170718
 %  paper: ICMEW2017
-%  TERMS OF USE : 
+%  terms of use : 
 %  Any scientific work that makes use of our code should appropriately
 %  mention this in the text and cite our ICMEW 2017 paper. For commercial
 %  use, please contact us.
+%  bibtex to cite:
+%  @inproceedings{jia2017multi,
+    title={Multi-occlusion handling in depth estimation of light fields},
+    author={Jia, Ying and Li, Weihai},
+    booktitle={Multimedia \& Expo Workshops (ICMEW), 2017 IEEE International Conference on},
+    pages={13--18},
+    year={2017},
+    organization={IEEE}
+    }
+%  primary references:
+%  Ting-Chun Wang, Alexei A. Efros, and Ravi Ramamoorthi.
+%  Occlusion-aware depth estimation using light-field cameras. 
+%  In Proceedings of International Conference on Computer Vision (ICCV), 2015.
 clc
 clear
 file_path     = 'F:/ICME2017/Code/input/tao_dataset/IMG_2.jpg';
